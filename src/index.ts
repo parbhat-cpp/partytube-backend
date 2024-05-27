0f73fe0804dc51ccf0433985ad286bed98338fb9
parbhat-cpp <prabhat29ps@gmail.com>
2024-05-27 20:12:58 +0530

    disconnect

--- src/index.ts
+++ src/index.ts
@@ -195,6 +195,20 @@ io.on("connection", (socket) => {
     socket.on("seek-video", (seekData: any) => {
         socket.broadcast.emit("set-video-duration", seekData);
     });
+
+    socket.on("disconnect", (reason, details) => {
+        // the reason of the disconnection, for example "transport error"
+        console.log(reason);
+
+        // the low-level reason of the disconnection, for example "xhr post error"
+        console.log(details.message);
+
+        // some additional description, for example the status code of the HTTP response
+        console.log(details.description);
+
+        // some additional context, for example the XMLHttpRequest object
+        console.log(details.context);
+    });
 });
 
 server.listen(WS_PORT);
